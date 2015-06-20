5033e9a5603c275804dbd6d9b72afce01ca48a45
Mario A Chavez <mario.chavez@gmail.com>
2015-06-20 13:29:06 -0500

    Add integration tests

---
+++ test/features/add_time_track_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+feature 'Add Time Track' do
+  scenario 'sanity' do
+    visit root_path
+
+    page.must_have_content 'Blog project'
+  end
+
+  scenario 'display project info' do
+    visit root_path
+    click_link 'Blog project'
+
+    page.must_have_content 'Registrar tiempo'
+  end
+
+  scenario 'display error when submitting empty form' do
+    visit root_path
+    click_link 'Blog project'
+
+    within('#new_time_track') do
+      click_button 'Crear Actividad'
+    end
+
+    page.must_have_content 'Por favor revise los datos de la forma'
+  end
+
+  scenario 'track new time entry' do
+    visit root_path
+    click_link 'Blog project'
+
+    within('#new_time_track') do
+      select 'Write post', from: 'Tarea'
+      fill_in 'Tiempo', with: '3'
+      click_button 'Crear Actividad'
+    end
+
+    page.wont_have_content 'Por favor revise los datos de la forma'
+    page.must_have_content 'Write post'
+    page.must_have_content '3.0 hrs'
+  end
+end
